06f67c809f1a6ad1d47b7eda6b5be3478e79aff2
xiangdepeng <xiangdepeng@outlook.com>
2019-12-17 19:17:42 +0800

    乘积最大子序列

---
+++ maximumProductSubarray/src/app/App.java
@@ -0,0 +1,22 @@
+/*
+乘积最大子序列
+https://leetcode-cn.com/problems/maximum-product-subarray/
+*/
+
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // int[] A = {84,-37,32,40,95};
+        int[] A = {1,2,3,-4,5,6};
+        int K = 6;
+        System.out.println(new Solution().maxProduct(A));
+    }
+}
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        System.out.println("hello");
+        return 0;
+    }
+}
