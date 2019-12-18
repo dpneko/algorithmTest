06dc29b26040b307ec8c344bd6af8bea1ff69142
xiangdepeng <xiangdepeng@guazi.com>
2019-12-18 15:38:58 +0800

    shortestSubarray

--- maximumProductSubarray/src/app/App.java
+++ maximumProductSubarray/src/app/App.java
@@ -7,9 +7,7 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // int[] A = {84,-37,32,40,95};
         int[] A = {1,2,3,-4,5,6};
-        int K = 6;
         System.out.println(new Solution().maxProduct(A));
     }
 }
